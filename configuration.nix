892371ddd2ae9352ba4466c88dd227dc1e470c41
Lukas Ejvinsson <lukas@ejvinsson.se>
2026-09-12 20:06:40 +0200

    Add gamescope

--- configuration.nix
+++ configuration.nix
@@ -159,6 +159,7 @@
   ];
 
   programs.steam.enable = true;
+  programs.steam.gamescopeSession.enable = true;
   programs.gamemode.enable = true;
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
